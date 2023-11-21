2a5b1abc5944016b479a8830d0143e7dfd4d1259
itisdanish <ardentdew@gmail.com>
2023-11-21 15:12:53 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Node-Api-test
+ A simple Node.js script that fetches fruit names from the [Fruityvice API](https://www.fruityvice.com/api/fruit/all) based on user input.
---
+++ index.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+const readline = require('readline');
+
+const apiUrl = 'https://www.fruityvice.com/api/fruit/all';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Ask the user how many names they want
+rl.question('How many fruit names do you want? ', (numberOfNames) => {
+  // Close the readline interface
+  rl.close();
+
+  // Make the API request
+  axios.get(apiUrl)
+    .then(response => {
+      // Extracting only the specified number of names from the array of objects
+      const names = response.data.slice(0, numberOfNames).map(fruit => fruit.name);
+
+      // Logging the names
+      console.log(names);
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error.message);
+    });
+});
