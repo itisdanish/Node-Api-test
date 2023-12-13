2b57b8ed4acecfd32a2d905bc8e6ef6678e18e29
itisdanish <ardentdew@gmail.com>
2023-12-13 15:31:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,47 @@
+// SIMPLE WAY
+
 const axios = require('axios');
-const readline = require('readline');
 
 const apiUrl = 'https://www.fruityvice.com/api/fruit/all';
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+axios.get(apiUrl)
+  .then(response => {
+    // Extracting only the names from the array of objects
+    const names = response.data.map(fruit => fruit.name);
 
-// Ask the user how many names they want
-rl.question('How many fruit names do you want? ', (numberOfNames) => {
-  // Close the readline interface
-  rl.close();
+    // Logging the names
+    console.log(names);
+  })
+  .catch(error => {
+    // Handle errors
+    console.error('Error fetching data:', error.message);
+  });
 
-  // Make the API request
-  axios.get(apiUrl)
-    .then(response => {
-      // Extracting only the specified number of names from the array of objects
-      const names = response.data.slice(0, numberOfNames).map(fruit => fruit.name);
+// const axios = require('axios');
+// const readline = require('readline');
 
-      // Logging the names
-      console.log(names);
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error.message);
-    });
-});
+// const apiUrl = 'https://www.fruityvice.com/api/fruit/all';
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// // Ask the user how many names they want
+// rl.question('How many fruit names do you want? ', (numberOfNames) => {
+//   // Close the readline interface
+//   rl.close();
+
+//   // Make the API request
+//   axios.get(apiUrl)
+//     .then(response => {
+//       // Extracting only the specified number of names from the array of objects
+//       const names = response.data.slice(0, numberOfNames).map(fruit => fruit.name);
+
+//       // Logging the names
+//       console.log(names);
+//     })
+//     .catch(error => {
+//       console.error('Error fetching data:', error.message);
+//     });
+// });
